36c61a066b120e2723d11410e5ebc08f0a13f7b1
Sara Cardoso <saracardoso501@gmail.com>
2021-06-25 19:18:19 +0100

    More results for new way of annotating Tcells (will be the final adopted one)

--- utils/modified_plots.R
+++ utils/modified_plots.R
@@ -1,12 +1,13 @@
-feature_plots = function(seurat_object, features, ncol=2, with_dimplot=T, dimplot_group='seurat_clusters'){
+feature_plots = function(seurat_object, features, ncol=2, with_dimplot=T, dimplot_group='seurat_clusters', point_size=0.2){
   feature_plots = list()
   for(feature in features){
-    feature_plots[[feature]] = Seurat::FeaturePlot(seurat_object, features=feature, pt.size=0.2) +
+    feature_plots[[feature]] = Seurat::FeaturePlot(seurat_object, features=feature, pt.size=point_size) +
       ggplot2::scale_color_gradientn(colours=c("navy", "yellow"), na.value=grDevices::rgb(0.75, 0.75, 0.75, alpha=0.2), limits=c(1e-5,NA)) +
       ggplot2::theme_minimal()
   }
   if(with_dimplot) feature_plots[['dimplot']] = Seurat::DimPlot(seurat_object, reduction="umap", group.by=dimplot_group,
-                                                                label=TRUE, label.size=6, pt.size=.2) + ggplot2::theme_minimal()
+                                                                label=TRUE, label.size=3, pt.size=point_size) + ggplot2::theme_minimal() + 
+                                                        Seurat::NoLegend()
   patchwork::wrap_plots(feature_plots, ncol=ncol)
 }
 
@@ -15,10 +16,10 @@ violin_plots = function(seurat_object, features, ncol=2, with_dimplot=T, group.b
   violin_plots = list()
   for(feature in features){
     violin_plots[[feature]] = Seurat::VlnPlot(seurat_object, features=feature, group.by=group.by, pt.size=0) +
-      ggplot2::theme_minimal() + Seurat::NoLegend()
+      ggplot2::theme_minimal() + Seurat::NoLegend() + Seurat::RotatedAxis()
   }
   if(with_dimplot) violin_plots[['dimplot']] = Seurat::DimPlot(seurat_object, reduction="umap", group.by=group.by,
-                                                                label=TRUE, label.size=6, pt.size=.2) + ggplot2::theme_minimal() +
+                                                                label=TRUE, label.size=3, pt.size=.2) + ggplot2::theme_minimal() +
                                                       Seurat::NoLegend()
   patchwork::wrap_plots(violin_plots, ncol=ncol)
 }
